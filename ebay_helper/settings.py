871984a0d20b34efc0a17e3dba34e6e6fb9915a4
Maxim Karamurzin <mkaramurzin02@gmail.com>
2022-08-15 14:01:03 -0700

    added heroku web hosting

--- ebay_helper/settings.py
+++ ebay_helper/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dc+!yf0dhe$26=lshqz1a1482tp1@^db9+d^6s#$1yq7&914&u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','ebayhelpertool.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
